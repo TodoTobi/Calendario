8317ca85547755e8f8b38b15e8e413ac86a8355e
TodoTobi <154088695+TodoTobi@users.noreply.github.com>
2025-08-06 15:29:19 -0300

    refactor: clean project and fix imports

--- Calendario/src/components/Calendar/Task.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Task({ task, onClick }) {
-  return (
-    <div className={`task ${task.category}`} onClick={onClick}>
-      {task.text}
-    </div>
-  );
-}
--- src/components/Calendar/WeekDisplay.jsx
+++ src/components/Calendar/WeekDisplay.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-codex/rename-multitaskfrom.jsx-to-multitaskform.jsx
-import { monthNames } from "../../utils/dateUtils";
-=======
 import { monthNames } from "../utils/dateUtils";
-main
 
 export default function WeekDisplay({ currentWeek }) {
   const [y, m, w] = currentWeek.split("_");
